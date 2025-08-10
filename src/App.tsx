6642c475136529989a1b436ffd3634baacad618a
Dibas K Borborah <dibas9110@gmail.com>
2025-08-10 12:21:56 +0530

    fear: background selection

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ function App() {
         <section className="p-4 flex flex-col gap-6 items-center justify-center h-full">
           <h3 className="text-2xl text-zinc-300">Plexdeeper</h3>
           <Textarea
-            placeholder="Ask anything or @mention a Space "
-            className="w-full max-w-2xl h-24 resize-none focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:border-zinc-600 shadow-none focus-ring-0 !bg-perplexity-text-color text-zinc-400 placeholder:text-zinc-500 rounded-xl !text-sm border-[1px]"
+            placeholder="Ask me anything..."
+            className="w-full max-w-2xl h-24 resize-none focus-visible:ring-0 focus-visible:ring-offset-0 border-zinc-700 focus-visible:border-zinc-600 shadow-none focus-ring-0 !bg-perplexity-text-color text-white/80 placeholder:text-zinc-500 rounded-xl border-[1px] manrope-font !text-base"
           />
         </section>
       </main>
